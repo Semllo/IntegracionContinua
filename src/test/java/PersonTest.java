04940563c8057b8b2b943ad734133857f7ea46c5
Marc <marcsemperlloret@gmail.com>
2022-05-19 15:35:14 +0200

    Initial commit

---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    @Test
+    void getName() {
+        Person p = new Person("James");
+        assertEquals("James", p.getName());
+    }
+}
